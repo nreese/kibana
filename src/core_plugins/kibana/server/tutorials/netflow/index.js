6c907b211684812908622657f4a23ba18799cb37
Shaunak Kashyap <ycombinator@gmail.com>
2017-11-08 16:42:36 -0800

    Incorporated writing style suggestions

--- src/core_plugins/kibana/server/tutorials/netflow/index.js
+++ src/core_plugins/kibana/server/tutorials/netflow/index.js
@@ -6,10 +6,12 @@ export function netflowSpecProvider() {
     id: 'netflow',
     name: 'Netflow',
     category: TUTORIAL_CATEGORY.SECURITY,
-    shortDescription: 'The Logstash Netflow module simplifies the collection, normalization, and visualization of network flow data',
-    longDescription: 'The Logstash Netflow module simplifies the collection, normalization, and visualization of network flow data.' +
-      ' With a single command, the module parses network flow data, indexes the events into Elasticsearch, and installs a suite of Kibana' +
-      ' dashboards to get you exploring your data immediately. Logstash modules support Netflow Version 5 and 9.',
+    shortDescription: 'Collect Netflow records sent by a Netflow exporter',
+    longDescription: 'The Logstash Netflow module simplifies the collection, normalization, and visualization of network flow data. ' +
+      'With a single command, the module parses network flow data, indexes the events into Elasticsearch, and installs a suite of Kibana ' +
+      'dashboards to get you exploring your data immediately. Logstash modules support Netflow Version 5 and 9. [Learn more]' +
+      '({config.elastic_docs.website_url}/guide/en/logstash/{config.elastic_docs.link_version}/netflow-module.html) about the Netflow ' +
+      'module.',
     //iconPath: '', TODO
     completionTimeMinutes: 10,
     //previewImagePath: 'kibana-apache.png', TODO
@@ -30,11 +32,9 @@ export function netflowSpecProvider() {
             instructions: [
               {
                 title: 'Download and install Logstash',
-                textPre: 'Download and install Logstash by running the commands below.' +
-                         ' Skip this step if you already have Logstash installed.' +
-                         ' If you are installing Logstash for the first time, we recommend reading the [Getting Started' +
-                         ' guide]({config.elastic_docs.website_url}/guide/en/logstash/{config.elastic_docs.link_version}' +
-                         '/getting-started-with-logstash.html) in the online documentation.',
+                textPre: 'Skip this step if Logstash is already installed. First time using Logstash? See the ' +
+                         '[Getting Started Guide]({config.elastic_docs.website_url}/guide/en/logstash/{config.elastic_docs.link_version}' +
+                         '/getting-started-with-logstash.html).',
                 commands: [
                   'curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-{config.kibana.version}.tar.gz',
                   'tar xzvf logstash-{config.kibana.version}.tar.gz'
@@ -42,17 +42,16 @@ export function netflowSpecProvider() {
               },
               {
                 title: 'Setup the Netflow module',
-                textPre: 'In the Logstash install directory, run the following command to enable the Netflow module.',
+                textPre: 'In the Logstash install directory, run the following command to setup the Netflow module.',
                 commands: [
                   './bin/logstash --modules netflow --setup',
                 ],
-                textPost: '*Note:* The --setup option creates a `netflow-*` index pattern in Elasticsearch and imports' +
-                  ' Kibana dashboards and visualizations. Running `--setup` is a one-time setup step. Omit this option' +
+                textPost: 'The --setup option creates a `netflow-*` index pattern in Elasticsearch and imports' +
+                  ' Kibana dashboards and visualizations. Running `--setup` is a one-time setup step. Omit this step' +
                   ' for subsequent runs of the module to avoid overwriting existing Kibana dashboards.'
               },
               {
-                title: 'Start the Netflow module',
-                textPre: 'Start Logstash with the Netflow module.',
+                title: 'Start Logstash',
                 commands: [
                   './bin/logstash --modules netflow -M netflow.var.input.udp.port={params.netflow.var.input.udp.port}'
                 ]
